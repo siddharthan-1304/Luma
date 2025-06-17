c442ddedfb2ad89bac238c3a842c871e13e7220c
siddharthan-1304 <sivastar1342002@gmail.com>
2025-06-17 15:30:17 +0530

    its time

---
+++ KuttyDragon/src/juSu/Juse.java
@@ -0,0 +1,11 @@
+package juSu;
+
+public class Juse {
+	
+	public static void main(String args[]) {
+		
+	System.out.println("is love");	
+		
+	}
+
+}
---
+++ dragon/src/dragon/DaDa.java
@@ -0,0 +1,10 @@
+package dragon;
+
+public class DaDa {
+	public static void main(String args[]) {
+		
+		
+		System.out.println("is fake");
+	} 
+
+}
